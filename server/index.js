8ae4d185818fd8718e4cf98f3458039f6b586b4d
Veronika <zhigunova1995@gmail.com>
2022-06-09 17:40:41 +0300

    configure connection with server

--- server/index.js
+++ server/index.js
@@ -1,9 +1,16 @@
 import express from "express";
+import sequelize from "./db";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-app.listen(PORT, () => {
-    console.log(`server started on port ${PORT}`)
-})
+sequelize
+  .sync()
+  .then((data) => {
+    app.listen(PORT, () => {
+      console.log(`server started on port ${PORT}`);
+    });
+  })
+  .catch((e) => {
+    console.log(e);
+  });
