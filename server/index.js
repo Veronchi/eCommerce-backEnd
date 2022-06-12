ae9360c57c2aa31393bdc11926e9508fbdde09ae
Veronika <zhigunova1995@gmail.com>
2022-06-12 18:49:19 +0300

    implement error handling

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import cors from 'cors'
 import sequelize from "./db";
-import userRoute from './resource/user/user.routes'
+import userRoute from './resource/user/user.routes';
+import errorHadler from './middleware/ErrorHandlingMiddleware';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use('/user', userRoute);
+app.use('/api/user', userRoute);
+
+
+app.use(errorHadler);
 
 // sequelize.authenticate();
 
--- server/resource/user/user.routes.js
+++ server/resource/user/user.routes.js
@@ -1,8 +1,10 @@
 import Express from "express";
-import userService from "./user.service";
+import * as userService from "./user.service";
+import ApiError from "../../error/ApiError";
+
 const router = Express.Router();
 
-router.post("/", async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const user = await userService.read(req.body);
     res.json(user);
@@ -11,7 +13,7 @@ router.post("/", async (req, res, next) => {
   }
 });
 
-router.get("/", async (req, res, next) => {
+router.post("/", async (req, res, next) => {
   try {
     const user = await userService.create(req.body);
     res.json(user);
