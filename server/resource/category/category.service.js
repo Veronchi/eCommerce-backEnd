a9a245c36e58a136b001364fb48e8b73d9881e01
Veronika <zhigunova1995@gmail.com>
2022-06-13 20:20:05 +0300

    implement category service

---
+++ server/resource/category/category.service.js
@@ -0,0 +1,39 @@
+import { Category } from "../common/models";
+
+async function create(categoryData) {
+  return await Category.create(categoryData);
+}
+
+async function getAll() {
+  return await Category.findAll();
+}
+
+async function getOne(categoryData) {
+  const category = await Category.findOne({
+    where: {
+      name: categoryData.name,
+    },
+  });
+  if(category === null) return [];
+
+  return category;
+}
+
+async function update(categoryData) {
+  console.log(categoryData.id)
+  return await Category.update(categoryData, {
+    where: {
+      id: categoryData.id,
+    },
+  });
+}
+
+async function remove(categoryData) {
+  return await Category.destroy({
+    where: {
+      id: categoryData.id,
+    },
+  });
+}
+
+export { create, getAll, getOne, update, remove };
