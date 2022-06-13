dc697ba86360f604757f5dc106399b960974d661
Veronika <zhigunova1995@gmail.com>
2022-06-13 20:21:49 +0300

    implement routes for categories

--- server/index.js
+++ server/index.js
@@ -1,7 +1,8 @@
 import express from "express";
-import cors from 'cors'
+import cors from "cors";
 import sequelize from "./db";
-import userRoute from './resource/user/user.routes';
+import userRoute from "./resource/user/user.routes";
+import categoryRoute from "./resource/category/category.routes";
 import errorHadler from './middleware/ErrorHandlingMiddleware';
 
 const app = express();
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use('/api/user', userRoute);
+app.use('/api/category', categoryRoute);
 
 
 app.use(errorHadler);
---
+++ server/resource/category/category.routes.js
@@ -0,0 +1,51 @@
+import Express from "express";
+import * as categoryService from "./category.service";
+
+const router = Express.Router();
+
+router.post('/', async (req, res, next) => {
+  try {
+    const category = await categoryService.create(req.body);
+    return res.json(category);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/', async(req, res, next) => {
+  try {
+    const categories = await categoryService.getAll(req.body);
+    return res.json(categories);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get('/get-category', async(req, res, next) => {
+  try {
+    const category = await categoryService.getOne(req.body);
+    return res.json(category);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.patch('/', async(req, res, next) => {
+  try {
+    const category = await categoryService.update(req.body);
+    return res.json(category);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete('/', async(req, res,next) => {
+  try {
+    const category = await categoryService.remove(req.body);
+    return res.json(category);
+  } catch (error) {
+    next(error);
+  }
+});
+
+export default router;
