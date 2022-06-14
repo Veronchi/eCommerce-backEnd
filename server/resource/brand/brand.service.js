e36c4a69a6cad406b2a5d064f06ee842f1e4446c
Veronika <zhigunova1995@gmail.com>
2022-06-14 16:03:04 +0300

    implement brands service

---
+++ server/resource/brand/brand.service.js
@@ -0,0 +1,38 @@
+import { Brand } from "../common/models";
+
+async function create(brandData) {
+  return await Brand.create(brandData);
+}
+
+async function getAll() {
+  return await Brand.findAll();
+}
+
+async function getOne(brandData) {
+  const brand = await Brand.findOne({
+    where: {
+      name: brandData.name,
+    },
+  });
+  if(brand === null) return [];
+
+  return brand;
+}
+
+async function update(brandData) {
+  return await Brand.update(brandData, {
+    where: {
+      id: brandData.id,
+    },
+  });
+}
+
+async function remove(brandData) {
+  return await Brand.destroy({
+    where: {
+      id: brandData.id,
+    },
+  });
+}
+
+export { create, getAll, getOne, update, remove };
