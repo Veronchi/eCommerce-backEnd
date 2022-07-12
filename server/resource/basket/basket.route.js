849531940db342ad4437c66c77fa8c81657561b6
Veronika <zhigunova1995@gmail.com>
2022-07-12 17:29:15 +0300

    add getting products route

--- server/resource/basket/basket.route.js
+++ server/resource/basket/basket.route.js
@@ -4,13 +4,22 @@ import authMiddleware from "../../middleware/authMiddleware";
 
 const router = Express.Router();
 
-router.post("/", authMiddleware, async(req, res, next) =>{
+router.post("/", authMiddleware, async (req, res, next) => {
   try {
     const result = await basketService.addProduct(req);
     return res.json(result);
   } catch (error) {
     next(error);
   }
-})
+});
+
+router.get("/", authMiddleware, async (req, res, next) => {
+  try {
+    const result = await basketService.getProducts(req);
+    return res.json(result);
+  } catch (error) {
+    next(error)
+  }
+});
 
 export default router;
