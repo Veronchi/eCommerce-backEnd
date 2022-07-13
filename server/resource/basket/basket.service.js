2db270eae48fc69e1fa9fb3a0783a7040f989bdd
Veronika <zhigunova1995@gmail.com>
2022-07-13 18:58:47 +0300

    implement get products from basket

--- server/resource/basket/basket.service.js
+++ server/resource/basket/basket.service.js
@@ -1,4 +1,4 @@
-import { Basket, Basket_product } from "../common/models";
+import { Basket, Basket_product, Product } from "../common/models";
 import Jwt from "jsonwebtoken";
 import ApiError from "../../error/ApiError";
 
@@ -16,4 +16,25 @@ async function addProduct(basketData) {
   return "Item added to the cart";
 }
 
-export { addProduct };
+async function getProducts(basketData) {
+  const token = basketData.headers.authorization.split(" ")[1];
+  const user = Jwt.verify(token, process.env.SECRET_KEY);
+  const { id } = await Basket.findOne({ where: { UserId: user.id } });
+  const basket = await Basket_product.findAll({ where: { BasketId: id } });
+
+  const basketArr = [];
+
+  for (let i = 0; i < basket.length; i++) {
+    const productToBasket = await Product.findOne({
+      where: {
+        id: basket[i].ProductId,
+      },
+    });
+
+    basketArr.push(productToBasket);
+  }
+
+  return basketArr;
+}
+
+export { addProduct, getProducts };
